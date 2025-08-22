fbb48d78ae651e3a17daf8e55fb28146bdb3a2e1
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-08-21 23:23:44 -0400

    first commit

---
+++ src/main/java/br/com/rodrigobsjava/dio/java_bank/JavaBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigobsjava.dio.java_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigobsjava/dio/java_bank/JavaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigobsjava.dio.java_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
